b3c3823fb25abae266438d82c7f14ca2dea16121
firdoshh19 <firdoshh34@gmail.com>
2026-03-02 23:15:52 +0530

    Update LM B Q10.cpp

--- LM B Q10.cpp
+++ LM B Q10.cpp
@@ -1,21 +1,28 @@
 //11.Write a program to check whether a character is a vowel or consonant.
-#include<iostream>
+#include <iostream>
 using namespace std;
-   int main (){
-       char ch;
-      cout<<"Enter an alphabet : ";
-      cin>>ch;
-   //   ch=tolower(ch);
 
-      if((ch>='a' && ch<='z')){
-        if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u')
-            cout<<"It is a vowel. ";
-            else
-                cout<<"It is a constant. ";
-      }
-      else{
-        cout<<"Enter a valid character ";
-      }
+int main()
+{
+    char ch;
 
+    cout << "Enter a character: ";
+    cin >> ch;
 
-   }
+    ch = tolower(ch);   // Convert character to lowercase
+
+    if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u')
+    {
+        cout << "Vowel";
+    }
+    else if(ch >= 'a' && ch <= 'z')
+    {
+        cout << "Consonant";
+    }
+    else
+    {
+        cout << "Not an alphabet";
+    }
+
+    return 0;
+}
