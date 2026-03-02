0e9cc2e411dea4b65ba1b94d1bdeedaae50d46c1
firdoshh19 <firdoshh34@gmail.com>
2026-03-02 22:59:30 +0530

    Update LM B Q9.cpp

--- LM B Q9.cpp
+++ LM B Q9.cpp
@@ -1,3 +1,16 @@
 /*9. Write a program to calculate the average of two integers and
 display the result as a floating-point value using explicit type
 casting.*/
+
+#inculde<iostream>
+using namespace std;
+int main (){
+int num1,num2;
+float average;
+cout<<"Enter two numbers ";
+cin>>num1>>num2;
+average=(float)(num1+num2)/2;
+cout<<"Result:"<<average;
+
+
+}
